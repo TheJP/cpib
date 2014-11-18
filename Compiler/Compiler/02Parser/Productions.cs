bb18850450ae11347b59d65dea0c1d5333b973b1
thejp <jp_55555@hotmail.com>
2014-11-18 14:27:07 +0100

    Started work on parser

---
+++ Compiler/Compiler/02Parser/Productions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler
+{
+    public class Productions
+    {
+        public IDictionary<NotTerminals, IDictionary<Terminals, Symbol[]>> ParseTable { get; private set; }
+        public Productions()
+        {
+            ParseTable = new Dictionary<NotTerminals, IDictionary<Terminals, Symbol[]>>();
+            ParseTable[NotTerminals.expr] = new Dictionary<Terminals, Symbol[]>(); //for each NTS
+            ParseTable[NotTerminals.expr][Terminals.ADDOPR] = new Symbol[]{ //For each Production
+                new Symbol(Terminals.ADDOPR, t => {}),
+                new Symbol(NotTerminals.expr, t => {})
+            };
+        }
+    }
+    public interface Expr : Treenode { } //For each NTS
+    public class ExprAddopr : Expr { } //For each Production
+}
---
+++ Compiler/Compiler/02Parser/Symbol.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler
+{
+    /// <summary>
+    /// A symbol is either a Terminal and a callback or a NotTerminal and a callback
+    /// </summary>
+    public class Symbol
+    {
+        public Terminals? Terminal { get; private set; }
+        public NotTerminals? NotTerminal { get; private set; }
+        public Action<Treenode> Callback { get; set; }
+
+        public Symbol(Terminals terminal, Action<Treenode> callback)
+        {
+            this.Terminal = terminal;
+            this.NotTerminal = null;
+            this.Callback = callback;
+        }
+        public Symbol(NotTerminals notTerminal, Action<Treenode> callback)
+        {
+            this.Terminal = null;
+            this.NotTerminal = notTerminal;
+            this.Callback = callback;
+        }
+    }
+}

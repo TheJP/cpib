84067d01b5787691845e1d6111bd1f9d91a0ea59
Ralf Grubenmann <ralf.grubenmann+github@gmail.com>
2015-01-03 16:32:08 +0100

    Implements logical NOT and int/decimal negation

---
+++ Compiler/Compiler/02Parser/AST/ASTNot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Compiler
+{
+    public class ASTNot : ASTExpression
+    {
+        public ASTExpression Expr { get; set; }
+
+        public override int GenerateCode(int loc, IVirtualMachine vm, CheckerInformation info)
+        {
+            loc = Expr.GenerateCode(loc, vm, info);
+
+            var type = GetExpressionType(info);
+
+            if (type == Type.BOOL || type == Type.INT32)
+            {
+                vm.IntInv(loc++);
+            }
+            else if (type == Type.DECIMAL)
+            {
+                vm.DecimalInv(loc++);
+            }
+            else
+            {
+                throw new IVirtualMachine.InternalError(
+                            "Cannot negate Non-Bool value " + Expr.ToString());
+            }
+
+            return loc;
+        }
+
+        public override Type GetExpressionType(CheckerInformation info)
+        {
+            return Expr.GetExpressionType(info);
+        }
+    }  
+}
